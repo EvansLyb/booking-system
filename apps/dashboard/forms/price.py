1a3462b0ab1747817a7c0f42fb5777fbb08de37e
Kyle <kyle.mai@wiredcraft.com>
2023-06-13 11:18:00 +0800

    alert full_date_price

--- apps/dashboard/forms/price.py
+++ apps/dashboard/forms/price.py
@@ -54,7 +54,7 @@ class PriceForm(ModelForm):
             format="%H:%M"
         ))
     full_day_price = forms.FloatField(
-        required=True,
+        required=False,
         widget=forms.TextInput(
             attrs={
                 "placeholder": "Full Day Price",
