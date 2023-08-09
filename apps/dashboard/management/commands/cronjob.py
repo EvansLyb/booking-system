120ba66a129471344372b8652fa8fdc693c0f491
Kyle <kyle.mai@wiredcraft.com>
2023-08-09 12:55:48 +0800

    cronjob - main logic

--- apps/dashboard/management/commands/cronjob.py
+++ apps/dashboard/management/commands/cronjob.py
@@ -2,23 +2,39 @@ import os, sys, time, datetime
 import threading
 from django.core.management.base import BaseCommand
 
-from apps.dashboard.models import Order, UnpaidOrder
+from apps.dashboard.models import Order, UnpaidOrder, OrderStatus
+from utils.order import check_if_order_is_cancellable, unfreeze
+from utils.util import trans_list_str_to_list
 
+
+UNPAID_ORDER_SURVIVAL_TIME = 15
 
 def automatic_cancellation_of_unpaid_orders():
     while True:
         try:
-            loca_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print('local time：'+str(loca_time))
-            order = Order.objects.all().first()
-            print('test cronjob')
-            print(order.id)
-            print(order.phone_number)
-            print(order.status)
-            print(order.date)
-            print(order.time_list)
-            order.remark = loca_time
-            order.save()
+            now = datetime.datetime.now()
+            local_time = now.strftime('%Y-%m-%d %H:%M:%S')
+            print('local time：' + str(local_time))
+            unpaid_order_list = UnpaidOrder.objects.all()
+            if unpaid_order_list and len(unpaid_order_list) > 0:
+                for unpaid_order in unpaid_order_list:
+                    order = Order.objects.get(id=unpaid_order.order_id)
+                    if order.status != OrderStatus.PENDING_PAYMENT:
+                        unpaid_order.delete()
+                    else:
+                        if (now - order.created_at).seconds > UNPAID_ORDER_SURVIVAL_TIME:
+                            # cancel this order
+                            is_cancellable = check_if_order_is_cancellable(order)
+                            if is_cancellable:
+                                order.status = OrderStatus.CANCELLED
+                                order.save()
+                                """ === data unfreeze === """
+                                unfreeze(order.facility_id, order.date, trans_list_str_to_list(order.time_list), order.court_type)
+                            else:
+                                errmsg = "order {} is not cancellable".format(order.id)
+                                print("Failed: automatic_cancellation_of_unpaid_orders")
+                                print(errmsg)
+                            unpaid_order.delete()
             time.sleep(1)
         except Exception as e:
             print('Error: automatic_cancellation_of_unpaid_orders')
@@ -29,8 +45,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            task1 = threading.Thread(target=automatic_cancellation_of_unpaid_orders)
-            task1.start()
+            automatic_cancellation_of_unpaid_orders()
         except Exception as e:
             print('Error: cronjob')
             print(str(e))
